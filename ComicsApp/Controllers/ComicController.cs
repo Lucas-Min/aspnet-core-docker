4cbc8bc997c11758dede1de9fc5218c4cc9d6bc0
lucas minardi <lucas.minardi@hotmail.com>
2018-05-08 02:15:55 -0300

    Initial commit

---
+++ ComicsApp/Controllers/ComicController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ComicsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComicsApp.Controllers
+{
+    public class ComicController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Comic> comics = new List<Comic>();
+
+            comics.Add(new Comic
+            {
+                DateRelease = DateTime.Now,
+                Id = 1,
+                Name = "Spiderman",
+                Price = 123
+            });
+
+            comics.Add(new Comic
+            {
+                DateRelease = new DateTime(1999, 12, 12),
+                Id = 2,
+                Name = "Captain America",
+                Price = 323
+            });
+
+            comics.Add(new Comic
+            {
+                DateRelease = new DateTime(2012, 06, 14),
+                Id = 3,
+                Name = "Ironman",
+                Price = 2323
+            });
+
+            return View(comics);
+        }
+    }
+}
---
+++ ComicsApp/Models/Comic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComicsApp.Models
+{
+    public class Comic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateRelease { get; set; }
+        public int Price { get; set; }
+    }
+}
